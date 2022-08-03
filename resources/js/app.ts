9f84af4333c1c4998856a08f8c414e51aa330fd6
chrystiannascimento <48285849+chrystiannascimento@users.noreply.github.com>
2022-08-02 21:40:06 -0300

    Initial commit

---
+++ resources/js/app.ts
@@ -0,0 +1,35 @@
+import './bootstrap';
+import '../css/app.css';
+
+import Vue from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+import ziggy from 'ziggy-js';
+
+/** Application Name */
+const appName =
+  window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+  title: title => `${title} - ${appName}`,
+  resolve: name =>
+    // @ts-ignore
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob('./Pages/**/*.vue')
+    ),
+  setup({ el, app, props, plugin }) {
+    // Add route function.
+    Vue.mixin({ methods: { route: ziggy } });
+    Vue.use(plugin);
+    // @ts-ignore
+    Vue.use(ZiggyVue, Ziggy);
+    const App = new Vue({ render: h => h(app, props) });
+    return App.$mount(el);
+  },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
